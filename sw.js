af8f49535e36fdec4d0af30194a3b15a3970fdf8
mariokarajallo <mariokarajallo92@gmail.com>
2023-05-27 21:01:26 -0400

    feat: add cache file

--- sw.js
+++ sw.js
@@ -1,7 +1,25 @@
+//anadiendo archivos para cache
+const nombreCache = "apv-v1";
+const archivosCache = [
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./css/bootstrap.css",
+  "./js/app.js",
+  "./js/apv.js",
+];
+
 //cuando se instala el service worker
 self.addEventListener("install", (e) => {
   console.log("Instalando el Service Worker");
   console.log(e);
+
+  e.waitUntil(
+    caches.open(nombreCache).then((cache) => {
+      console.log("cacheando");
+      cache.addAll(archivosCache);
+    })
+  );
 });
 
 // Activar el Service worker
