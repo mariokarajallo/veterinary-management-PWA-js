343488eec9cf39fa54508835063fc7e18c4c063e
mariokarajallo <mariokarajallo92@gmail.com>
2023-05-28 22:36:47 -0400

    feat: add service worker

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
 //anadiendo archivos para cache
-const nombreCache = "apv-v1";
+const nombreCache = "apv-v3";
 const archivosCache = [
   "./",
+  "./error.html",
   "./index.html",
   "./css/styles.css",
   "./css/bootstrap.css",
   "./js/app.js",
   "./js/apv.js",
+  "./manifest.json",
 ];
 
 //cuando se instala el service worker
 self.addEventListener("install", (e) => {
   console.log("Instalando el Service Worker");
-  console.log(e);
 
   //espera que se descarguen todos los archivos del cache
   e.waitUntil(
@@ -26,17 +27,33 @@ self.addEventListener("install", (e) => {
 // Activar el Service worker
 self.addEventListener("activate", (e) => {
   console.log("Service Worker Activado");
-  console.log(e);
+
+  //actualizamos nuestro cache con el ultimo cambio realizado
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== nombreCache)
+          .map((key) => caches.delete(key)) //borra los demas caches antiguos
+      );
+    })
+  );
 });
 
 // evento fetch para descargar archivos staticos
 self.addEventListener("fetch", (e) => {
-  console.log("fetch...", e);
+  console.log("Fetch...", e);
 
   //mostrar todo el archivo que tenemos en cache cuando estamos Offline
   e.respondWith(
-    caches.match(e.request).then((respuestaCache) => {
-      return respuestaCache;
-    })
+    caches
+      .match(e.request)
+      .then((respuestaCache) => {
+        return respuestaCache || fetch(e.request);
+      })
+      .catch(() => {
+        return caches.match("/error.html");
+        // console.log("Error al obtener la página de error");
+      })
   );
 });
